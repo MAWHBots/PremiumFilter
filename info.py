2f4bb105f77e364a707bbc534d5ac069e49c8193
MAWHBots <160210473+MAWHBots@users.noreply.github.com>
2024-07-12 11:16:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '27434372'))
-API_HASH = environ.get('API_HASH', 'a0ad66eae1be3ab8987df1de8e129400')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+API_ID = int(environ.get('API_ID', '20763817'))
+API_HASH = environ.get('API_HASH', '07186e8f2ffe607e99eedf7eaa5e630b')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7131188454:AAFdrrzUsmNo-fqJaj3dsi_RErBQhRISOqs')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
